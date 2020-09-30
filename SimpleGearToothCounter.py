53d61c49eb03f385deccc843a65c514784c41384
Simon Ameye <66630402+simon-ameye@users.noreply.github.com>
2020-09-30 18:53:49 +0200

    Update SimpleGearToothCounter.py

--- SimpleGearToothCounter.py
+++ SimpleGearToothCounter.py
@@ -16,9 +16,10 @@ from tkinter.filedialog import askopenfilename
 from tkinter import Tk
 from pyautogui import screenshot
 from skimage import img_as_ubyte
+import matplotlib.image as mpimg
 
-def Gear_function (im,Sdist,Soff,im_for_color):
-    color_values = np.array([np.mean(im_for_color[:,:,0]),np.mean(im_for_color[:,:,1]),np.mean(im_for_color[:,:,2])])
+def Gear_function (im,Sdist,Soff):
+    color_values = im[clicks[1, 1], clicks[1, 0]][0:3]
     color_values[color_values==0]=1e-5
     L = im.shape[1]
     H = im.shape[0]
@@ -53,14 +54,14 @@ def Gear_function (im,Sdist,Soff,im_for_color):
     if len(peaks) == 0:peaks = np.array([0])
     sorted_peaks_args = np.flip(peaks[np.argsort(np.abs(X_small[peaks]))])
     sorted_peaks_args = np.append(sorted_peaks_args,np.zeros(1000)).astype("int")
-    return center_point,r,bnim_centered,x_small,X_small,x_bary,bary,sorted_peaks_args,N
+    return center_point,r,bnim_centered,x_small,X_small,x_bary,bary,sorted_peaks_args,N,color_values
 
 def update(val):
     Sdist = Dist_slider.val
     Sdist = np.power((Sdist)/(Sdist-1),2)*3
     Sval = Val_slider.val
     Soff = Off_slider.val
-    center_point,r,bnim_centered,x_small,X_small,x_bary,bary,sorted_peaks_args,N = Gear_function(im,Sdist,Soff,im_for_color)
+    center_point,r,bnim_centered,x_small,X_small,x_bary,bary,sorted_peaks_args,N,color_values = Gear_function(im,Sdist,Soff)
     nb_of_teeth = x_small[sorted_peaks_args[np.int(Sval)]]
     real_graph.clear()
     real_graph.imshow(im)
@@ -69,7 +70,7 @@ def update(val):
     real_graph.axis('off')
     real_graph.set_title('Original image', fontsize = 10, color = "black")
     bn_graph.clear()
-    pic = bn_graph.imshow(bnim_centered, cmap=colors.ListedColormap(['tab:blue', 'white', 'orange']))
+    pic = bn_graph.imshow(bnim_centered, cmap=colors.ListedColormap([color_values/255, 'white', 'orange']))
     pic.set_clim(-1,1)
     bn_graph.axis('off')
     bn_graph.set_title('Filtered image', fontsize = 10, color = "black")
@@ -102,39 +103,15 @@ def update(val):
     tempo_graph.plot(angles, np.cos(init_angle + angles * nb_of_teeth) * np.abs(X_small[sorted_peaks_args[np.int(Sval)]]) + np.mean(bary),  color = "orange")
     for txt in fig.texts:
         txt.remove()
+    real_graph.plot(clicks[1,0], clicks[1,1], marker = "1", color = "red", markersize = 10)
     freq_graph.annotate(str(nb_of_teeth), (nb_of_teeth,0), textcoords="offset points", xytext=(0,-5), ha='center', va='top', color='orange', fontsize=16)
     fig.canvas.draw_idle()
-
-def ROI(iminit):
-    # Select ROI for image crop
-    print("2) Crop the region of interest and press Enter")
-    showCrosshair = False
-    fromCenter = False
-    window_height = 1000
-    cv2.namedWindow("Region of interest", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("Region of interest", window_height,int(iminit.shape[0]/iminit.shape[1]*window_height))
-    r = cv2.selectROI("Region of interest",iminit,fromCenter,showCrosshair)
-    cv2.destroyAllWindows()
-    im = iminit[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
-    # Display cropped image
-    # Select ROI for color picker
-    print("3) Select a region for the gear color and press Enter")
-    cv2.namedWindow("Region for the gear color", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("Region for the gear color", window_height,int(im.shape[0]/im.shape[1]*window_height))
-    r = cv2.selectROI("Region for the gear color",im,fromCenter,showCrosshair)
-    # Crop image
-    im_for_color = im[int(r[1])+1:int(r[1]+r[3]-1), int(r[0])+1:int(r[0]+r[2])-1]
-    im_for_color = im_for_color[:,:,:3]
-    # Display cropped image
-    cv2.destroyAllWindows()
-    return im,im_for_color
 
 def takeScreenshot(self): 
     global im,im_for_color
     print("1) A Screenshot has been performed")
     myScreenshot = screenshot()
-    iminit = img_as_ubyte(myScreenshot)
-    (im,im_for_color) = ROI(iminit)
+    im = img_as_ubyte(myScreenshot)
     update("val")
     
 def browseim(self):
@@ -144,15 +121,28 @@ def browseim(self):
     print("1) Browse your image")
     curr_directory = os.getcwd()
     filename = askopenfilename(initialdir = curr_directory + "/examples", title = "Select picture")
-    iminit = imread(filename)[:,:,:4]
-    (im,im_for_color) = ROI(iminit)
+    im = imread(filename)[:,:,:4]
     update("val")
+    
+def onclick(event): 
+    global clicks
+    try : 
+        print("button=%d, x=%d, y=%d, xdata=%f, ydata=%f" % ( 
+         event.button, event.x, event.y, event.xdata, event.ydata))
+        clicks[event.button - 1, :] = [event.xdata, event.ydata]
+        print(clicks)
+        update("val")
+    except : 
+        print("Click on pic")
 
 plt.close('all')
 #initialize
 plt.rcParams['toolbar'] = 'None'
 fig = plt.figure("A Simple Gear Tooth Counter") 
 Sdist = 0.37
+clicks = np.array([[0, 0], #left click = crop top left
+          [186, 237], #midle click = color clic
+          [0, 0]]) #right click = crop bottom right
 gs = fig.add_gridspec(15, 2)
 real_graph = fig.add_subplot(gs[2:10,0])
 bn_graph = fig.add_subplot(gs[2:10,1])
@@ -179,4 +169,10 @@ bscreenshot.on_clicked(takeScreenshot)
 axnext = plt.axes([0.2, 0.025, 0.2, 0.035])
 bbrowse = wgt.Button(axnext, 'Browse')
 bbrowse.on_clicked(browseim)
+
+clic = real_graph.figure.canvas.mpl_connect('button_press_event', onclick)
+
+
+
+
 plt.show()
